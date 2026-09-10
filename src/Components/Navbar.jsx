8f8753371d5addeab6dac2aed30e8dd64c39e05e
Garrett Uichanco <developmentunit@Garretts-Mac-mini.local>
2026-09-10 15:53:08 -0700

    bug fix: fixed navbar not updating invert settings when clicking on link and moving to different page without scrolling a bit first.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { assets } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
     const [scrolled, setScrolled] = useState(false)
     const [isDarkSection, setIsDarkSection] = useState(true)
+    const location = useLocation()
 
     useEffect(() => {
         const updateNavbarTheme = () => {
@@ -37,7 +38,7 @@ const Navbar = () => {
             window.removeEventListener('scroll', updateNavbarTheme)
             window.removeEventListener('resize', updateNavbarTheme)
         }
-    }, [])
+    }, [location.pathname])
 
     const isLightText = isDarkSection
 
